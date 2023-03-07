8eae1ccbcd63ec264a6a5bc8661b9ba492f72d47
Rohit Poduri <rohitpoduri09@gmail.com>
2023-03-07 11:30:35 -0600

    Implement five_dirs shell script

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+mkdir five
+for i in {1..5}
+do
+	mkdir five/dir$i
+	for j in {1..4}
+	do
+		touch five/dir$i/file$j.txt
+		for k in $(seq $j)
+		do
+			echo $j >> five/dir$i/file$j.txt
+		done
+	done
+done
