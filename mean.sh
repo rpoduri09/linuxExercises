3098aaef324e9d2715ef6aa2bcb56496ab09c7bb
Rohit Poduri <rohitpoduri09@gmail.com>
2023-03-07 13:01:02 -0600

    small edits to mean script

--- mean.sh
+++ mean.sh
@@ -12,6 +12,6 @@ else
   file=$2
 fi
 
-mean=$(cut -d',' -f$column $file | tail -n +2 | { sum=0; count=0; while read value; do sum=$((sum+value)); count=$((count+1)); done; echo "$sum/$count" | bc -l; })
+mean=$(cut -d ',' -f $column $file | tail -n +2 | { sum=0; count=0; while read value; do sum=$((sum+value)); count=$((count+1)); done; echo "$sum/$count" | bc -l; })
 
 echo "Mean of column $column: $mean"
