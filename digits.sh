0737e3d3c730b14ece1b420e65fe14f120ff12ec
Rohit Poduri <rohitpoduri09@gmail.com>
2023-03-09 01:13:32 -0600

    Finish implementing school and digits scripts

--- digits.sh
+++ digits.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+sum=0
+
+for i in {1000..2000}; do
+  if [[ "$i" =~ ^[01]+$ ]]; then
+	  echo $i
+	  sum=$((sum + i))
+  fi
+done
+
+echo "The sum of the numbers between 1000 and 2000 having digits only from the set {0, 1} is $sum."
--- school.sh
+++ school.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | { sum=0; count=0; while read val; do sum=$((sum+val)); count=$((count+1)); done; echo "Average TotalAssessedValue: $(bc -l <<<"$sum/$count")"; }
